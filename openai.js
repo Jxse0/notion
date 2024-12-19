5fec35c1068f083e185d44a0a6f786e888e53e0e
Jose Exposito Javier <jose.expositojavier@cgi.com>
2024-12-19 17:10:47 +0100

    klappt

---
+++ openai.js
@@ -0,0 +1,27 @@
+import { writeFileSync } from "node:fs";
+import OpenAI from "openai";
+import { readFileSync } from "node:fs";
+
+const openai = new OpenAI({ apiKey: process.env["OPENAI_API_KEY"] });
+
+const textContent = readFileSync("structured_text.txt", "utf-8");
+
+const response = await openai.chat.completions.create({
+  model: "gpt-4o-audio-preview",
+  modalities: ["text", "audio"],
+  audio: { voice: "alloy", format: "wav" },
+  messages: [
+    {
+      role: "user",
+      content: "Fasse zusammen:" + textContent,
+    },
+  ],
+});
+
+console.log(response.choices[0]);
+
+writeFileSync(
+  "notion.wav",
+  Buffer.from(response.choices[0].message.audio.data, "base64"),
+  { encoding: "utf-8" }
+);
